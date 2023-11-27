52523f094ee7c4f9ab888aa7e04281caf53ace96
SousMour <142849312+SousMour@users.noreply.github.com>
2023-11-27 11:28:59 -0300

    Add files via upload

---
+++ S14_T2_QUESTAO_3.py
@@ -0,0 +1,23 @@
+def cria_lista():
+    lista = []
+    for i in range(20):
+        numeros = int(input())
+        lista.append(numeros)
+
+    return lista
+def elimina_repetidos(lista):
+    lista_sem_repeticao = []
+
+    for i in lista:
+        if i not in lista_sem_repeticao:
+            lista_sem_repeticao.append(i)
+
+    return lista_sem_repeticao
+
+def main():
+    lista = cria_lista()
+    lita_sem_repetidos = elimina_repetidos(lista)
+
+    print(f'{lita_sem_repetidos}')
+if __name__=="__main__":
+    main()
---
+++ S14_T2_QUESTAO_5.py
@@ -0,0 +1,36 @@
+def cria_lista():
+    lista = []
+    for i in range(10):
+        numeros = int(input())
+        lista.append(numeros)
+    return lista
+
+
+def uniao_listas_sem_repetidos(lista_um, lista_dois):
+    lista_unida_sem_repeticao = []
+    lista_unida = []
+
+    for i in range(10):
+        lista_unida.append(lista_um[i])
+
+    for i in range(10):
+
+        lista_unida.append(lista_dois[i])
+
+    for y in lista_unida:
+        if y not in lista_unida_sem_repeticao:
+            lista_unida_sem_repeticao.append(y)
+
+    return lista_unida_sem_repeticao
+
+
+def main():
+    lista_um = cria_lista()
+    lista_dois = cria_lista()
+
+    resultado = uniao_listas_sem_repetidos(lista_um, lista_dois)
+    print(resultado)
+
+
+if __name__ == "__main__":
+    main()
